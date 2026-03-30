aa9144f8eb43e40cf1202b700dff2d00aef51105
Jobson de Oliveira Coutinho <jobsondeveloper@gmail.com>
2026-03-30 16:56:48 -0300

    Feature: Adds the project structure and basic configurations

---
+++ src/main/java/br/com/delivery/micro/DeliveryApplication.java
@@ -0,0 +1,17 @@
+package br.com.delivery.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@SpringBootApplication
+@EnableMongoAuditing
+@EnableFeignClients
+public class DeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryApplication.class, args);
+	}
+
+}
