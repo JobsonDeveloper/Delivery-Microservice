e396455fa396bf3d54f73115955e908358d8b847
Jobson de Oliveira Coutinho <jobsondeveloper@gmail.com>
2026-04-09 17:30:10 -0300

    Feature: Add a reaction of the canceled sale

---
+++ src/main/java/br/com/delivery/micro/config/KafkaConsumerConfig.java
@@ -0,0 +1,80 @@
+package br.com.delivery.micro.config;
+
+import br.com.delivery.micro.event.dto.SaleEventDto;
+import br.com.delivery.micro.event.dto.PaymentEventDto;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+    @Value("${spring.kafka.consumer.bootstrap-servers}")
+    private String kafkaUrl;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String microserviceGroup;
+
+    @Bean
+    public ConsumerFactory<String, PaymentEventDto> paymentConsumerFactory() {
+
+        JsonDeserializer<PaymentEventDto> deserializer =
+                new JsonDeserializer<>(PaymentEventDto.class);
+
+        deserializer.addTrustedPackages("*");
+
+        return new DefaultKafkaConsumerFactory<>(
+                Map.of(
+                        ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaUrl,
+                        ConsumerConfig.GROUP_ID_CONFIG, microserviceGroup
+                ),
+                new StringDeserializer(),
+                deserializer
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, PaymentEventDto> paymentKafkaListenerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, PaymentEventDto> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(paymentConsumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, SaleEventDto> saleConsumerFactory() {
+
+        JsonDeserializer<SaleEventDto> deserializer =
+                new JsonDeserializer<>(SaleEventDto.class);
+
+        deserializer.addTrustedPackages("*");
+
+        return new DefaultKafkaConsumerFactory<>(
+                Map.of(
+                        ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaUrl,
+                        ConsumerConfig.GROUP_ID_CONFIG, microserviceGroup
+                ),
+                new StringDeserializer(),
+                deserializer
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, SaleEventDto> saleKafkaListenerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, SaleEventDto> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(saleConsumerFactory());
+        return factory;
+    }
+}
